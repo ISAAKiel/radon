867a708765d3d0b9b34e4e010b448ec8dfb46f52
martin <martin.hinz@ufg.uni-kiel.de>
2016-06-27 13:30:15 +0200

    adjusted scaffold generator settings

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,14 @@ module Radon
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :test_unit, fixture: false
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+    end
   end
 end
