c6cc4c4d95348dab0395a1af37cf174e1a7d4379
martin <martin.hinz@ufg.uni-kiel.de>
2016-06-30 10:16:04 +0200

    reenabled calibration

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -399,7 +399,7 @@ render :layout => false
     def calibrate_multi
 #    params[:ids]=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
     order=params[:sort_by]
-    @samples = Sample.find params[:ids], :order=>order
+    @samples = Sample.order(order).where(id: params[:ids])
 
     out="Options(){RawData=TRUE};Plot(){"
     @samples.each do |sample|
@@ -512,7 +512,7 @@ render :layout => false
     def calibrate_sum
 #    params[:ids]=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
     order=params[:sort_by]
-    @samples = Sample.find params[:ids], :order=>order
+    @samples = Sample.order(order).where(id: params[:ids])
 #    @sample_names_string = @samples.map(&:name).to_sentence
     out="Options(){RawData=TRUE};Plot(){Sum(){"
     @samples.each do |sample|
--- spec/support/users_helper.rb
+++ spec/support/users_helper.rb
@@ -1,14 +1,14 @@
 module UserHelper
   
   def login(a)
-    visit root_path
-    fill_in 'user_session[login]', with: a.login
-    fill_in 'user_session[password]', with: a.password
-    click_button 'Sign in'
+    visit login_path
+    fill_in 'login_page_login_field', with: a.login
+    fill_in 'login_page_password_field', with: a.password
+    click_button 'Submit'
   end
   
   def logout(a)
-    visit root_path
-    click_link 'Log Out'
+    visit logout_path
+#    click_link 'Log Out'
   end
 end
