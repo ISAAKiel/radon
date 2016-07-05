468736528387792e40121ed9a894b11dba0831cd
martin <martin.hinz@ufg.uni-kiel.de>
2016-07-05 12:28:29 +0200

    maintainance

---
+++ app/controllers/announcements_controller.rb
@@ -0,0 +1,68 @@
+class AnnouncementsController < ApplicationController
+  filter_access_to :index, :show, :edit, :new, :create, :update, :destroy 
+  before_action :set_announcement, only: [:show, :edit, :update, :destroy]
+
+  # GET /announcements
+  def index
+    @announcements = Announcement.all
+  end
+
+  # GET /announcements/1
+  def show
+  end
+
+  # GET /announcements/new
+  def new
+    @announcement = Announcement.new
+  end
+
+
+  # GET /announcements/1/edit
+  def edit
+  end
+
+  # POST /announcements
+  def create
+    @announcement = Announcement.new(announcement_params)
+
+    if @announcement.save
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+      end
+      client.update("New Announcement on RADON: " + url_for(@announcement))
+    
+      redirect_to @announcement, notice: 'Announcement was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  # PATCH/PUT /announcements/1
+  def update
+    if @announcement.update(announcement_params)
+      redirect_to @announcement, notice: 'Announcement was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  # DELETE /announcements/1
+  def destroy
+    @announcement.destroy
+    redirect_to announcements_url, notice: 'Announcement was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_announcement
+      @announcement = Announcement.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def announcement_params
+      params.require(:announcement).permit(:title, :content)
+    end
+end
