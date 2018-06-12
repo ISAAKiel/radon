02e0f890845aa1bb53830d06c620e7675e0a0135
nevrome <clemens@nevrome.de>
2018-06-12 17:32:46 +0200

    changes in docker start script: nginx have to be started last

--- docker/rails/start.sh
+++ docker/rails/start.sh
@@ -2,15 +2,16 @@
 export SECRET_KEY_BASE=$(rake secret) && \
     #rake db:migrate && \
     rake assets:precompile --trace && \
+    # the following symbolic links have to be created to bypass the asset pipeline
+    # when including the specific assets of radon and radon-b
+    # radon
+    ln -s app/assets/images/oxford_photo.jpg public/assets/oxford_photo.jpg && \
+    ln -s app/assets/images/c14_s.gif public/assets/c14_s.gif && \
+    ln -s app/assets/images/c14_s.png public/assets/c14_s.png && \
+    # radon-b
+    ln -s app/assets/images/radon_b_image.png public/assets/radon_b_image.png && \
+    ln -s app/assets/images/radon_b_logo.png public/assets/radon_b_logo.png && \
+    ln -s app/assets/images/radon_b_schriftzug.png public/assets/radon_b_schriftzug.png && \
+    # start webserver
     /opt/nginx/sbin/nginx && \
-		# the following symbolic links have to be created to bypass the asset pipeline
-		# when including the specific assets of radon and radon-b
-		# radon
-		ln -s app/assets/images/oxford_photo.jpg public/assets/oxford_photo.jpg && \
-		ln -s app/assets/images/c14_s.gif public/assets/c14_s.gif && \
-		ln -s app/assets/images/c14_s.png public/assets/c14_s.png && \
-		# radon-b
-		ln -s app/assets/images/radon_b_image.png public/assets/radon_b_image.png && \
-		ln -s app/assets/images/radon_b_logo.png public/assets/radon_b_logo.png && \
-		ln -s app/assets/images/radon_b_schriftzug.png public/assets/radon_b_schriftzug.png
 
