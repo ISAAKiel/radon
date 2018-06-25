72cbd8e9e2f9380b4c0136c6c661492b6827c48b
Clemens Schmid <clemens@nevrome.de>
2018-06-25 14:17:31 +0200

    forced symlink creation
    The docker start script also has to complete on container restart and should not fail due to already available links

--- docker/rails/start.sh
+++ docker/rails/start.sh
@@ -5,13 +5,13 @@ export SECRET_KEY_BASE=$(rake secret) && \
     # the following symbolic links have to be created to bypass the asset pipeline
     # when including the specific assets of radon and radon-b
     # radon
-    ln -s ../../app/assets/images/oxford_photo.jpg public/assets/oxford_photo.jpg && \
-    ln -s ../../app/assets/images/c14_s.gif public/assets/c14_s.gif && \
-    ln -s ../../app/assets/images/c14_s.png public/assets/c14_s.png && \
+    ln -sf ../../app/assets/images/oxford_photo.jpg public/assets/oxford_photo.jpg && \
+    ln -sf ../../app/assets/images/c14_s.gif public/assets/c14_s.gif && \
+    ln -sf ../../app/assets/images/c14_s.png public/assets/c14_s.png && \
     # radon-b
-    ln -s ../../app/assets/images/radon_b_image.png public/assets/radon_b_image.png && \
-    ln -s ../../app/assets/images/radon_b_logo.png public/assets/radon_b_logo.png && \
-    ln -s ../../app/assets/images/radon_b_schriftzug.png public/assets/radon_b_schriftzug.png && \
+    ln -sf ../../app/assets/images/radon_b_image.png public/assets/radon_b_image.png && \
+    ln -sf ../../app/assets/images/radon_b_logo.png public/assets/radon_b_logo.png && \
+    ln -sf ../../app/assets/images/radon_b_schriftzug.png public/assets/radon_b_schriftzug.png && \
     # start webserver
     /opt/nginx/sbin/nginx
 
